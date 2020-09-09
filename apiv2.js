4c6108852e21f08dbb5496527f4084ea7e9c36a8
Nguyen Thanh Trieu <trieunguyenthanh@admicro.vn>
2020-09-09 10:29:01 +0700

    update code

--- apiv2.js
+++ apiv2.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
-const getDataCovid19FromApi= async (city = 'Tokyo') => {
+const getDataWeatherFromApi= async (city = 'Tokyo') => {
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=84f0c05e16abc57b03ca8fa00b59f78e&units=metric&lang=vi`;
   const response = await axios.get(url);
   const data = await response.status === 200 ? response.data : [];
   return data;
 }
 const main = async () => {
-  const data = await getDataCovid19FromApi('HaNoi');
+  const data = await getDataWeatherFromApi('HaNoi');
   
   console.log(data);
 }
